0d36b9d89147cbff2ef1d7d3bd545bdca30720e2
LidiaT <tothlidia80@gmail.com>
2018-09-29 13:03:37 +0200

    secret page

---
+++ www/js/index.js
@@ -0,0 +1,19 @@
+
+
+
+let indexView=new Vue ({
+    router,
+    el: '#indexHeader',
+    data: {
+        word: ''
+    },
+    methods: {
+        toAdminPage: function(){
+            
+            if(this.word==='admin'){
+                window.location.href='http://localhost:3000/admin.html';
+            }
+        }
+    }
+
+});
